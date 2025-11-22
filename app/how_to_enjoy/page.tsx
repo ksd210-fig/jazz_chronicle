299adc184c0cf991dc4e5b79b38cd0f24592c003
구수담 <ksd210@gmail.com>
2025-11-22 15:53:21 +0900

    how_to_enjoy 페이지 생성

---
+++ app/how_to_enjoy/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function HowToEnjoy() {
+  return(
+    <div>
+      <h1>How to Enjoy</h1>
+      <p>How to enjoy jazz</p>
+      <link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">How to enjoy jazz</link>
+    </div>
+  )
+}
