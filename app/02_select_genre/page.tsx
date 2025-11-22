741d006b068187caaefaa6a3f6788820263fd33b
구수담 <ksd210@gmail.com>
2025-11-22 21:17:16 +0900

    select_genre 데이터 연결

--- app/02_select_genre/page.tsx
+++ app/02_select_genre/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import dataList from "@/app/database";
+
 export default function SelectGenre() {
+  const [index, setIndex] = useState(0);
+
   return(
     <div>
       <h1>Select Genre</h1>
+      <div>
+        <p>Genre : {dataList[index].genre}</p>
+        <p>City : {dataList[index].city}</p>
+        <p>Year : {dataList[index].year}</p>
+        <p>Improvisation : {dataList[index].improvisation}</p>
+      </div>
+      <p>*Improvisation=즉흥성</p>
+      <div>
+        <button onClick={() => {
+          const newIndex = index - 1;
+          setIndex(newIndex < 0 ? dataList.length - 1 : newIndex);
+        }}>◀︎</button>
+        <Link href="/03_select_artist">Next</Link>
+        <button onClick={() => {
+          const newIndex = index + 1;
+          setIndex(newIndex >= dataList.length ? 0 : newIndex);
+          }}>▶︎</button>
+
+      </div>
     </div>
   )
 }
--- app/database.ts
+++ app/database.ts
@@ -3,7 +3,7 @@ const dataList: DataType[] = [
       genre: 1,
       city: 1,
       year: 1,
-      improvision: 1,
+      improvisation: 1,
       artist: ["1-1", "1-2", "1-3"],
       song: ["1-1", "1-2", "1-3"]
     },
@@ -11,7 +11,7 @@ const dataList: DataType[] = [
       genre: 2,
       city: 2,
       year: 2,
-      improvision: 2,
+      improvisation: 2,
       artist: ["2-1", "2-2", "2-3"],
       song: ["2-1", "2-2", "2-3"]
     }
