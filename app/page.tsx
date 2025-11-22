0089ba2d0e729f16e63aee60a17845369b00d7e9
구수담 <ksd210@gmail.com>
2025-11-22 16:54:58 +0900

    메인 페이지 레이아웃

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return(
     <div>
-      <h1>Hello World</h1>
+      <h1>Jazz Chronicle</h1>
+      <p>History of Jazz</p>
+      <Link href="/01_how_to_enjoy">Enter</Link>
+      <p>©2025 FIG.1 STUDIO</p>
     </div>
   )
 }
