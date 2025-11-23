4e09bb39be04c38c4f2d47281fb15276f9ebe971
구수담 <ksd210@gmail.com>
2025-11-23 10:56:37 +0900

    database 업데이트

--- app/database.ts
+++ app/database.ts
@@ -1,19 +1,59 @@
 const dataList: DataType[] = [
     {
-      genre: 1,
-      city: 1,
-      year: 1,
-      improvisation: 1,
-      artist: ["1-1", "1-2", "1-3"],
+      genre: "RAG TIME",
+      city: "Saint louis",
+      year: "1900",
+      improvisation: 2,
+      artist: ["Scott Joplin", "Jelly Roll Morton", "W.C Handy"],
       song: ["1-1", "1-2", "1-3"]
     },
     {
-      genre: 2,
-      city: 2,
-      year: 2,
-      improvisation: 2,
-      artist: ["2-1", "2-2", "2-3"],
+      genre: "NEW ORLEANS",
+      city: "New Orleans",
+      year: "1910",
+      improvisation: 4,
+      artist: ["Original Dixieland Jass Band", "Joe King Oliver", "New Orleans Rhythm Kings"],
       song: ["2-1", "2-2", "2-3"]
+    },
+    {
+      genre: "CHICAGO JAZZ",
+      city: "Chicago",
+      year: "1920",
+      improvisation: 3,
+      artist: ["Jelly Roll Morton", "Louis Armstrong & His Hot Five", "3-3"],
+      song: ["3-1", "3-2", "3-3"]
+    },
+    {
+      genre: "SWING JAZZ",
+      city: "KANAS CITY",
+      year: "1930",
+      improvisation: 3,
+      artist: ["Benny Goodman", "Glenn Miller", "Duke Ellington"],
+      song: ["4-1", "4-2", "4-3"]
+    },
+    {
+      genre: "BEBOP",
+      city: "New York",
+      year: "1940",
+      improvisation: 5,
+      artist: ["Dizzy Gillespie", "Thelonious Monk", "Charlie Parker"],
+      song: ["5-1", "5-2", "5-3"]
+    },
+    {
+      genre: "COOL JAZZ",
+      city: "Los Angeles",
+      year: "1950",
+      improvisation: 2,
+      artist: ["Miles Davis", "Dave Brubeck", "Chet Baker"],
+      song: ["6-1", "6-2", "6-3"]
+    },
+    {
+      genre: "HARD BOP",
+      city: "Philadelphia",
+      year: "1960",
+      improvisation: 4,
+      artist: ["Art Blakey & The Jazz Messengers", "John Coltrane", "Horace Silver"],
+      song: ["7-1", "7-2", "7-3"]
     }
   ];
   
